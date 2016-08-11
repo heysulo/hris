213b4e28b0d195d861b2bf12ad118a940c7a5e6d
Sulochana Kodituwakku <sulochana.456@live.com>
2016-08-11 18:19:00 +0530

    fixed sizes

---
+++ dashboard/index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<title>USER </title>
+    <link rel="stylesheet" type="text/css" href="../public/css/artista.css">
+    <link href='https://fonts.googleapis.com/css?family=Catamaran:400,200' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<div class="clearfix">
+		<?php include_once('templates/side_panel.php'); ?>
+		<?php include_once('templates/top_pane.php'); ?>
+	</div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -4,61 +4,62 @@
     <link rel="stylesheet" type="text/css" href="public/css/artista.css">
 </head>
 <body>
-<div id="overlay"></div>
-<div class="bg_blurredimg"></div>
-
-<div class="login_box_outer">
-
-    <center>
-
-        <img src="public/img/ucsc_logo.png">
-        <h1 class="head1" style="color:#000">Human Resource Information System</h1>
+    <div class="loginmaindiv">
         <div>
-            <p class="logindesc">A Human Resource Information System for University of Colombo School of Computing
-                which will enhance the communications between the academic staff and the students of UCSC</p>
-        </div>
-        <div class="login_box">
-            <form action="action_page.php">
-                <div>
-                    <div style="max-width:300px;">
-                        <center>
-                            <p class="anitxt1">Login to the Human Resource
-                            Information Center of UCSC as a registered member and access the full features.</p>
-                        </center>
-                    </div>
-                    <input class="txt_field" type="text" id="fname" name="firstname" placeholder="Email Address">
-                    <input class="txt_field" type="password" id="lname" name="lastname" placeholder="Password">
-                    <div class="spacerx"></div>
-                    <div class="spacerx"></div>
-                    <input type="button" class="user_choose_button" value="Login as Member">
-                    <div class="spacerx"></div>
+            <div class="introbox">
+                <center>
+                    <img src="public/img/ucsc_logo.png">
+                    <h1 class="head1" style="color:#000">Human Resource Information System</h1>
                     <div>
-                        <a class="txtassist" href="Google.lk" style="text-align: left;">Forgot your password</a>
-                        &nbsp; &nbsp; &nbsp; &nbsp;
-                        <a class="txtassist" href="Google.lk" style="text-align: right; ">Contact Administrator</a>
+                        <p class="logindesc">The Human Resource Information System is a place where you can access the shared information of the academic staff and the students of the University of Colombo School of Computing.
+<br><br>The Objective of this system is to enhance the communication between the academic staff, students and the other interested 3rd parties.</p>
                     </div>
-                    <div class="spacerx"></div>
-                    <div class="spacerx"></div>
-                    <hr class="hrstyle">
-                    <div class="spacerx"></div>
-                    <center style="font-size:13px;color:#5F5F5F;"><p class="anitxt1">Login to the Human Resource
-                            Information Center of UCSC as guest and access the skill directory.</p></center>
-                    <a href="app/view/templates/profile.php">
-                        <input type="button" class="user_choose_button" value="General Public"></a>
-                </div>
-            </form>
-        <div>
+                </center>
+            </div>
+        </div>
+        <center>
+            <div class="login_box">
+                <form action="action_page.php">
+                    <div>
+                        <div style="max-width:300px;">
+                            <center>
+                                <p class="anitxt1">Login to the Human Resource
+                                Information Center of UCSC as a registered member and access the full features.</p>
+                            </center>
+                        </div>
+                        <input class="txt_field" type="text" id="fname" name="firstname" placeholder="Email Address">
+                        <input class="txt_field" type="password" id="lname" name="lastname" placeholder="Password">
+                        <div class="spacerx"></div>
+                        <div class="spacerx"></div>
+                        <input type="button" class="user_choose_button" value="Login as Member">
+                        <div class="spacerx"></div>
+                        <div>
+                            <a class="txtassist" href="Google.lk" style="text-align: left;">Forgot your password</a>
+                            &nbsp; &nbsp; &nbsp; &nbsp;
+                            <a class="txtassist" href="Google.lk" style="text-align: right; ">Contact Administrator</a>
+                        </div>
+                        <div class="spacerx"></div>
+                        <div class="spacerx"></div>
+                        <hr class="hrstyle">
+                        <div class="spacerx"></div>
+                        <center style="font-size:13px;color:#5F5F5F;"><p class="anitxt1">Login to the Human Resource
+                                Information Center of UCSC as guest and access the skill directory.</p></center>
+                        <a href="./dashboard/index.php">
+                            <input type="button" class="user_choose_button" value="General Public"></a>
+                    </div>
+                </form>
+            </div>
+        </center> 
+    </div>
+    <div class="bottomline">
+        <center>
+        <p>All rights reserved by University of Colombo School of Computing<br>
+No: 35, Reid Avenue, Colombo 7, Sri Lanka.</p>
+        </center>
+    </div>
+    
 
-    </center>
-</div>
+    <div class="bg_blurredimg"></div>
 
-
-<script type="text/javascript" src="public/js/jquery-2.2.4.min.js" ></script>
-
-<script type="text/javascript">
-    $(window).load(function () {
-        $("#overlay").fadeOut();
-    });
-</script>
 </body>
 </html>
