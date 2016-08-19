115cc6f7c001dd74c2db55f79fe1fb35caf1831e
Emalsha <remalsha@gmail.com>
2016-08-20 00:31:06 +0530

    Dashboard page development started

--- app/templates/bottom_panel.php
+++ app/templates/bottom_panel.php
@@ -2,7 +2,9 @@
 
     <div class="dashboard_bottom"> 
     	<div class="dashboard_leftbox">
-            <?php include_once('box_availability_status.php'); ?>
+            <div class="dbox">
+                <?php include_once('box_availability_status.php'); ?>
+            </div>
             <div class="dbox"></div>
         </div>
         <div class="dashboard_rightbox">
