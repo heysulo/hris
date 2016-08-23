e0a4edf9725fff4b108479dd01b31373ad138800
Sulochana Kodituwakku <sulochana.456@live.com>
2016-08-23 18:23:16 +0530

    Availability status box completed

--- dashboard/templates/box_availability_status.php
+++ dashboard/templates/box_availability_status.php
@@ -16,61 +16,63 @@
 
 
 
+
 				<div class="availability_dropdown_content">
 					<div class="availability_dropdown_item" >
 						<div style="width:20%;float:left;height:auto;">
-							<div class="saved_availability_icon" style="background-color:#0f0;"></div>
+							<div class="saved_availability_icon" style="background-color:#34a853;"></div>
 						</div>
-						<div style="width:60%;float:left;height:auto;margin:6px;padding-left:10px;">
+						<div class="availability_dropdown_item_text_template">
 							Available
 						</div>
 					</div>
 
 					<div class="availability_dropdown_item" >
 						<div style="width:20%;float:left;height:auto;">
-							<div class="saved_availability_icon" style="background-color:#0f0;"></div>
+							<div class="saved_availability_icon" style="background-color:#fbbc05;"></div>
 						</div>
-						<div style="width:60%;float:left;height:auto;margin:6px;padding-left:10px;">
-							Available
-						</div>
-					</div>
-
-					<div class="availability_dropdown_item" >
-						<div style="width:20%;float:left;height:auto;">
-							<div class="saved_availability_icon" style="background-color:#0f0;"></div>
-						</div>
-						<div style="width:60%;float:left;height:auto;margin:6px;padding-left:10px;">
-							Available
+						<div class="availability_dropdown_item_text_template">
+							Away
 						</div>
 					</div>
 
 					<div class="availability_dropdown_item" >
 						<div style="width:20%;float:left;height:auto;">
-							<div class="saved_availability_icon" style="background-color:#0f0;"></div>
+							<div class="saved_availability_icon" style="background-color:#ea4335;"></div>
 						</div>
-						<div style="width:60%;float:left;height:auto;margin:6px;padding-left:10px;">
-							Available
-						</div>
-					</div>
-
-					<div class="availability_dropdown_item" >
-						<div style="width:20%;float:left;height:auto;">
-							<div class="saved_availability_icon" style="background-color:#0f0;"></div>
-						</div>
-						<div style="width:60%;float:left;height:auto;margin:6px;padding-left:10px;">
-							Available
+						<div class="availability_dropdown_item_text_template">
+							Busy
 						</div>
 					</div>
 
 					<div class="availability_dropdown_item" >
 						<div style="width:20%;float:left;height:auto;">
-							<div class="saved_availability_icon" style="background-color:#0f0;"></div>
+							<div class="saved_availability_icon" style="background-color:#4285f4;"></div>
 						</div>
-						<div style="width:60%;float:left;height:auto;margin:6px;padding-left:10px;">
-							Available
+						<div class="availability_dropdown_item_text_template">
+							Lecture
 						</div>
-					</div>				
+					</div>
+
+					<div class="availability_dropdown_item" >
+						<div style="width:20%;float:left;height:auto;">
+							<div class="saved_availability_icon" style="background-color:#707070;"></div>
+						</div>
+						<div class="availability_dropdown_item_text_template">
+							Unavailable
+						</div>
+					</div>		
 				</div>
+			</div>
+			<div >
+				<select name="cars"  size="5" class="saved_status_messages">
+					  <option style="color:#34a853;" value="volvo">Available till 2.00 PM</option>
+					  <option style="color:#ea4335;" value="saab">Exam Duty</option>
+					  <option style="color:#ea4335;" value="opel">Board Meeting</option>
+					  <option style="color:#fbbc05;" value="volvo">Out for Lunch</option>
+					  <option style="color:#4285f4;" value="saab">Lecture at W002</option>
+				  <option value="audi">Audi</option>
+				</select>
 			</div>
 		</div>
 	</div>
