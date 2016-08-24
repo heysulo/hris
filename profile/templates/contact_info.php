9f6b7a105d8673830a8422caefdcab21826275e6
Sulochana Kodituwakku <sulochana.456@live.com>
2016-08-24 23:53:27 +0530

    wtf

---
+++ profile/templates/contact_info.php
@@ -0,0 +1,8 @@
+<div class="dbox profile_section_contactinfo">
+	<div class="dboxheader dbox_head_profilecontactinfo">
+		<div class="dboxtitle">
+			Contact Information
+		</div>
+			blur - Optional. This is the third value, and must be in pixels. Adds a blur effect to the shadow. A larger value will create more blur (the shadow becomes bigger and lighter). Negative values are not allowed. If no value is specified, 0 is used (the shadow's edge is sharp).
+	</div>
+</div>
