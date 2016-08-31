c90eb07e1fff5edb1939164f4b7269e719cfcf27
Emalsha <remalsha@gmail.com>
2016-08-31 08:57:40 +0530

    advanced search apply into php

---
+++ app/user/advancedSearch.php
@@ -0,0 +1,181 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emalsha
+ * Date: 8/31/16
+ * Time: 7:39 AM
+ */?>
+<!DOCTYPE html>
+<head>
+    <?php
+    define(hris_access,true);
+    require_once('../templates/path.php');
+    include('../templates/_header.php');
+    session_start();
+
+    if (!isset($_SESSION['email'])){
+        header("location:../../index.php");
+    }
+
+    $fname = $_SESSION['fname'];
+    $lname = $_SESSION['lname'];
+    $type  = $_SESSION['type'];
+    $email = $_SESSION['email'];
+    $pro_pic = $_SESSION['pro_pic'];
+
+    ?>
+    <title>HRIS | Groups</title>
+</head>
+<body>
+<div class="clearfix">
+    <?php include_once('../templates/navigation_panel.php'); ?>
+    <?php include_once('../templates/top_pane.php'); ?>
+
+    <!--Content goes here-->
+    <div class="bottomPanel">
+
+        <div style="float:left;width:auto;">
+            <div class="txt_paneltitle">Advanced Search</div>
+        </div>
+        <div class="profile_section_main">
+
+            <!--Basic details search options.-->
+            <div class="dbox advacnedsearch_section_basic">
+                <div class="dboxheader dbox_head_advancedsearch_basic">
+                    <div class="dboxtitle botmarg">
+                        Search People
+                    </div>
+                    <form>
+                        <div class="clearfloat">
+                            <input type="checkbox">First name: <input class="advancedsearch_textbox floatright" type="text" name="fname"><br>
+                        </div>
+                        <div class="clearfloat">
+                            <input type="checkbox">Last name: <input class="advancedsearch_textbox floatright" type="text" name="lname"><br>
+
+                        </div>
+                        <hr class="advancedsearch_hr">
+                        <input type="checkbox">Birthday :
+                        <select>
+                            <?php
+                            for($i = 1900; $i < 2016; ++$i) {
+                                echo "<option value='$i'>$i</option>";
+                            }
+                            ?>
+                        </select>
+                        <select>
+                            <?php
+                            for($i = 1; $i < 13; ++$i) {
+                                echo "<option value='$i'>$i</option>";
+                            }
+                            ?>
+                        </select><br>
+                        <input type="checkbox">Gender : <input type="radio" name="gender" value="male" checked> Male
+                        <input type="radio" name="gender" value="female"> Female<br>
+                        <hr class="advancedsearch_hr">
+                        <input type="checkbox">From : &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+                        <select>
+                            <?php
+                            $dist = "Jaffna,Kilinochchi,Mannar,Mullaitivu,Vavuniya,Puttalam,Kurunegala,Gampaha,Colombo,Kalutara,Anuradhapura,Polonnaruwa,Matale,Kandy,Nuwara Eliya,Kegalle,Ratnapura,Trincomalee,Batticaloa,Ampara,Badulla,Monaragala,Hambantota,Matara,Galle";
+                            $ary = explode(',', $dist);
+                            foreach($ary as $dist){
+                                echo "<option value='$dist'>$dist</option>";
+                            }
+                            ?>
+                        </select><br>
+                        <input type="checkbox">Lives in :&nbsp;&nbsp;
+                        <select>
+                            <?php
+                            $dist = "Jaffna,Kilinochchi,Mannar,Mullaitivu,Vavuniya,Puttalam,Kurunegala,Gampaha,Colombo,Kalutara,Anuradhapura,Polonnaruwa,Matale,Kandy,Nuwara Eliya,Kegalle,Ratnapura,Trincomalee,Batticaloa,Ampara,Badulla,Monaragala,Hambantota,Matara,Galle";
+                            $ary = explode(',', $dist);
+                            foreach($ary as $dist){
+                                echo "<option value='$dist'>$dist</option>";
+                            }
+                            ?>
+                        </select><br>
+                        <hr class="advancedsearch_hr">
+                        <div class="clearfloat">
+                            <input type="checkbox">Skills/Interests: <br>
+                            <div class="advancedsearch_interestpick_area">
+                            </div>
+                        </div>
+                        <hr class="advancedsearch_hr">
+                        <div class="clearfloat">
+                            <input type="checkbox">Clubs and Societies: <br>
+                            <div class="advancedsearch_interestpick_area">
+                            </div>
+                        </div>
+                        <hr class="advancedsearch_hr">
+                        <div class="clearfloat">
+                            <input type="checkbox">Languages: <br>
+                            <div class="advancedsearch_interestpick_area">
+                            </div>
+                        </div>
+                        <hr class="advancedsearch_hr">
+
+                        <div class="clearfloat">
+                            <input type="checkbox">Filter: <input class="advancedsearch_textbox floatright" type="text" name="fname"><br>
+                        </div>
+                        <br>
+
+                        <button class="default_button availability_status_button">Search</button>
+                    </form>
+                </div>
+            </div>
+
+            <!--Groups details search here-->
+            <div class="dbox advacnedsearch_section_basic">
+                <div class="dboxheader dbox_head_advancedsearch_basic">
+                    <div class="dboxtitle botmarg">
+                        Search Groups and Societies
+                    </div>
+                    <input class="group_searchbar" type="text" name="fname">
+                    <br>
+                    <br>
+                    <button class="default_button availability_status_button">Search</button>
+                </div>
+            </div>
+
+
+            <!--Group representative details search in here-->
+            <div class="dbox advacnedsearch_section_basic">
+                <div class="dboxheader dbox_head_advancedsearch_basic">
+                    <div class="dboxtitle botmarg">
+                        Search Society Representatives
+                    </div>
+                    Society :&nbsp;&nbsp;
+                    <select class="advancedsearch_socity_representive_dropdown">
+                        <?php
+                        $dist = "Jaffna,Kilinochchi,Mannar,Mullaitivu,Vavuniya,Puttalam,Kurunegala,Gampaha,Colombo,Kalutara,Anuradhapura,Polonnaruwa,Matale,Kandy,Nuwara Eliya,Kegalle,Ratnapura,Trincomalee,Batticaloa,Ampara,Badulla,Monaragala,Hambantota,Matara,Galle";
+                        $ary = explode(',', $dist);
+                        foreach($ary as $dist){
+                            echo "<option value='$dist'>$dist</option>";
+                        }
+                        ?>
+                    </select><br>
+
+                    <hr class="advancedsearch_hr">
+                    Role :&nbsp;&nbsp;
+                    <select class="advancedsearch_socity_representive_dropdown">
+                        <?php
+                        $dist = "Jaffna,Kilinochchi,Mannar,Mullaitivu,Vavuniya,Puttalam,Kurunegala,Gampaha,Colombo,Kalutara,Anuradhapura,Polonnaruwa,Matale,Kandy,Nuwara Eliya,Kegalle,Ratnapura,Trincomalee,Batticaloa,Ampara,Badulla,Monaragala,Hambantota,Matara,Galle";
+                        $ary = explode(',', $dist);
+                        foreach($ary as $dist){
+                            echo "<option value='$dist'>$dist</option>";
+                        }
+                        ?>
+                    </select><br>
+                    <br>
+                    <button class="default_button availability_status_button">Search</button>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</div>
+
+<?php
+include_once('../templates/_footer.php');
+?>
+</body>
+</html>
--- app/user/profile.php
+++ app/user/profile.php
@@ -81,11 +81,34 @@
                             Roles in Clubs and Socities
                         </div>
                     </div>
-                    <?php include("socity_item.php") ?>
-                    <?php include("socity_item.php") ?>
-                    <?php include("socity_item.php") ?>
-                    <?php include("socity_item.php") ?>
-                    <?php include("socity_item.php") ?>
+
+                    <!--Content info add from here-->
+                    <div class="society_item">
+                        <div class="society_item_club">Society : Gavel Club University of Colombo</div>
+                        <div class="society_item_role">My Role : Super Long President</div>
+                        <br>
+                        <div class="society_item_extra">
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
+                            Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                            Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+                            Nulla consequat massa quis enim.
+                        </div>
+                    </div>
+                    <hr>
+
+                    <!--Content info add from here-->
+                    <div class="society_item">
+                        <div class="society_item_club">Society : Gavel Club University of Colombo</div>
+                        <div class="society_item_role">My Role : Super Long President</div>
+                        <br>
+                        <div class="society_item_extra">
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
+                            Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                            Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+                            Nulla consequat massa quis enim.
+                        </div>
+                    </div>
+                    <hr>
                 </div>
 
                 <!--Skilled programming technology-->
@@ -100,7 +123,9 @@
                         $str =  "Af-Soomaali,Afrikaans,Azərbaycan dili,Bahasa Indonesia,Bahasa Melayu,Basa Jawa,Bisaya,Bosanski,Brezhoneg";
                         $ary = explode(',', $str);
                         foreach($ary as $str){
-                            include("language_item.php");
+                        echo "<div class='skill_item language_item'>
+	                                $str
+                              </div>";
                         }
                         ?>
                     </div>
@@ -118,10 +143,11 @@
                         </div>
 
                     </div>
-                    <?php include("personal_info_item.php") ?>
-                    <?php include("personal_info_item.php") ?>
-                    <?php include("personal_info_item.php") ?>
-                    <?php include("personal_info_item.php") ?>
+                    <!--Personal info content goes here.-->
+                    <div class="contact_info_item">
+                        <div class="contact_info_item_field">School :</div>
+                        <div class="contact_info_item_value">St. Aloysius' College, Galle</div>
+                    </div>
 
                 </div>
 
@@ -131,7 +157,15 @@
                         <div class="dboxtitle botmarg">
                             About Me
                         </div>
-                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc.</p>
+                        <p>     Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                            Aenean commodo ligula eget dolor.
+                            Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                            Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+                            Nulla consequat massa quis enim.
+                            Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
+                            In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
+                            Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.
+                            Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
                     </div>
                 </div>
 
@@ -147,13 +181,12 @@
                         $str =  "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.";
                         $ary = explode(' ', $str);
                         foreach($ary as $str){
-                            include("skill_item.php");
+                            echo "<div class='skill_item'>
+                                    $str
+                                  </div>";
                         }
                         ?>
 
-                        <?php include("skill_item.php") ?>
-                        <?php include("skill_item.php") ?>
-                        <?php include("skill_item.php") ?>
                     </div>
                 </div>
             </div>
