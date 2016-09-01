e36cdde77f1b3c2dfad8fc9740d7909c5ae8c4ff
Sulochana Kodituwakku <sulochana.456@live.com>
2016-09-01 21:53:23 +0530

    Password reset completed

---
+++ passwordReset/passwordReset.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+    <title>USER </title>
+    <link rel="stylesheet" type="text/css" href="../public/css/artista.css">
+    <link href="https://fonts.googleapis.com/css?family=Catamaran" rel="stylesheet">
+</head>
+<body class="welcome_body">
+<div class="welcome_top_gradient"></div>
+<script>
+    var step;
+    step = 1;
+</script>
+<div class="welcome_section_banner">
+    Password Reset
+</div>
+<div class="dbox welcome_section_maindbox">
+    <?php include ("./template/steps.php"); ?>
+    <div class="resetpassword_support_text">
+        In prder to reset your password please enter your Email address. This email address should be the same email address which is used to create your account.
+    </div>
+    <div style="text-align: center;">
+        <input id="fname" name="firstname" class="welcome_inputbox" placeholder="Email Address" type="email">
+
+        <input class="user_choose_button welcome_continue_button" value="Go to Dashboard" type="button">
+    </div>
+
+</div>
+
+
+
+</body>
+
+
+</html>
---
+++ passwordReset/verificationCode.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>USER </title>
+    <link rel="stylesheet" type="text/css" href="../public/css/artista.css">
+    <link href="https://fonts.googleapis.com/css?family=Catamaran" rel="stylesheet">
+</head>
+<body class="welcome_body">
+<div class="welcome_top_gradient"></div>
+<script>
+    var step;
+    step = 3;
+</script>
+<div class="welcome_section_banner">
+    Password Reset
+</div>
+<div class="dbox welcome_section_maindbox">
+    <?php include ("./template/steps.php"); ?>
+    <div class="resetpassword_support_text">
+        Please check your email for a message from HRIS conataining a code which is 8 digits long. Enter that code in order to continue the password reset process.<br><br> If you requested a new password but didn't receive your password-reset email check the spam or junk mail folder in your email account.
+    </div>
+    <div>
+        <?php include ("./template/invalidCode.php"); ?>
+    </div>
+    <div style="text-align: center;">
+        <input id="fname" name="firstname" class="welcome_inputbox resetpassword_verification_input" placeholder="########" type="number">
+
+        <input class="user_choose_button welcome_continue_button" value="Continue" type="button">
+    </div>
+
+</div>
+
+
+
+</body>
+
+
+</html>
