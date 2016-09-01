986c318f13b4b6c2bffce7f759200779e130ea47
Sulochana Kodituwakku <sulochana.456@live.com>
2016-09-01 15:00:37 +0530

    Completed profile setup

--- profileSetup/finalize.php
+++ profileSetup/finalize.php
@@ -6,7 +6,7 @@
 </head>
 <body class="welcome_body">
 <script>
-    var step = 2;
+    var step = 4;
 </script>
 <div class="welcome_section_banner">
     Welcome to HRIS UCSC !
@@ -14,35 +14,16 @@
 <div class="dbox welcome_section_maindbox">
     <?php include ("./template/steps.php"); ?>
     <div class="welcome_section_introtxt">
-        Enter your name name here, so the members of the system can recognize you. This name will be used everywhere inside the system including searches.
+        Enter your interests\skill and the languages that you are capable of speaking and writing. This information will be available for the members of the Human Resource Information System.
     </div>
-    <div style="text-align: center;">
-    <div class="welcome_profile_picture"><div class="welcome_new_propic">Upload Image</div></div>
-    <input type="text" id="fname" name="firstname" class="welcome_inputbox" placeholder="First Name"><br>
-    <input type="text" id="fname" name="firstname" class="welcome_inputbox" placeholder="Middle Name"><br>
-        <input type="text" id="fname" name="firstname" class="welcome_inputbox" placeholder="Last Name"><br>
     <?php
-
+        include ("./template/interetst.php");
+        echo "<hr class=\"welcome_profile_setup_hr\">";
+        include ("./template/languages.php");
     ?>
-        <hr class="advancedsearch_hr">
-        <div class="welcome_section_introtxt">
-            Select your Role and Gender. Also include your academic year so the members can easily find you inside the Human Resource Information System.
-        </div>
-        <div>
-            <select class="welcome_dropdown">
-                <option value="volvo">Student</option>
-                <option value="saab">Instructor</option>
-                <option value="opel">Lecturer</option>
-                <option value="audi">Academic Staff</option>
-            </select><br>
-            <input type="text" id="fname" name="firstname" class="welcome_inputbox" placeholder="Academic Year"><br>
-            <select class="welcome_dropdown">
-                <option value="volvo">Male</option>
-                <option value="saab">Female</option>
-            </select><br>
-        </div>
+    <div style="text-align: center;">
 
-        <input class="user_choose_button welcome_continue_button" value="Continue" type="button">
+        <input class="user_choose_button welcome_continue_button" value="Go to Dashboard" type="button">
     </div>
 
 </div>
