7ec9884fa11d84ab8b2e1fdde6206528160ebfd3
Sulochana Kodituwakku <sulochana.456@live.com>
2016-09-13 00:48:37 +0530

    Group page completed

---
+++ group/templates/post.php
@@ -0,0 +1,19 @@
+<div class="dbox group_dbox_post">
+    <div class="group_post_head">
+        <div class="group_post_user_image"></div>
+        <div class="group_post_head_content">
+            <div class="group_post_head_user_name">Sulochana Kodituwakku</div>
+            <div class="group_post_head_role">Vice Precident</div>
+            <div class="group_post_head_timestamp">12:09 AM Tuesday, September 13, 2016</div>
+        </div>
+        <div class="group_post_head_options">
+            <div class="group_post_head_options_item">Delete Post</div>
+            <div class="group_post_head_options_item">Pin Post</div>
+            <div class="group_post_head_options_item">Ban Member</div>
+        </div>
+    </div>
+    <div class="group_post_content">
+        Its time to look back on the tech in I/O 2016. I/O Rewind Colombo will happen on the 30th at Dialog Auditorium. Register here.
+        Please note that we will be giving priority to those who were not able to come to the IO extended.
+    </div>
+</div>
