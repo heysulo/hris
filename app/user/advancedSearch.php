b6050264e264c12185c4625d3233dc36f52be0d2
Sulochana Kodituwakku <sulochana.456@live.com>
2017-01-14 22:55:18 +0530

    profile edit

--- app/user/advancedSearch.php
+++ app/user/advancedSearch.php
@@ -9,6 +9,9 @@
 <head>
     <?php
     define('hris_access',true);
+    $conn = null;
+    require_once("./config.conf");
+    require_once("../database/database.php");
     require_once('../templates/path.php');
     include('../templates/_header.php');
     if (session_status() == PHP_SESSION_NONE) {
@@ -33,7 +36,7 @@
 <?php include_once('../templates/navigation_panel.php'); ?>
 <?php include_once('../templates/top_pane.php'); ?>
 <div class="bottomPanel">
-    <form>
+    <form method="get" action="searchresults.php">
     <div style="width:auto;">
         <div class="txt_paneltitle">Advanced Search</div>
     </div>
@@ -42,86 +45,54 @@
 
             <div class="dbox">
                 <div class="adv_search_subtitle">Name</div>
-                <input type="text" id="pw" name="password" class="adv_search_txtbox" placeholder="First Name" pattern="[A-Za-z]" style="width: 92%;">
-                <input type="text" id="pw" name="password" class="adv_search_txtbox" placeholder="Middle Name" pattern="[A-Za-z]" style="width: 92%;">
-                <input type="text" id="pw" name="password" class="adv_search_txtbox" placeholder="Last Name" pattern="[A-Za-z]" style="width: 92%;">
+                <input type="text" id="pw" name="fname" class="adv_search_txtbox" placeholder="First Name" pattern="[a-zA-Z]+" style="width: 92%;">
+                <input type="text" id="pw" name="mname" class="adv_search_txtbox" placeholder="Middle Name" pattern="[a-zA-Z]+" style="width: 92%;">
+                <input type="text" id="pw" name="lname" class="adv_search_txtbox" placeholder="Last Name" pattern="[a-zA-Z]+" style="width: 92%;">
             </div>
 
             <div class="dbox">
                 <div class="adv_search_subtitle">Birthday</div>
-                <input type="text" id="pw" name="password" class="adv_search_txtbox" placeholder="Year" pattern="[1|2]{1}[0-9]{1}[0-9]{1}[0-9]{1}" style="width: 25%;">
-                <input type="text" id="pw" name="password" class="adv_search_txtbox" placeholder="Month" pattern="[1-12]" style="width: 25%;">
-                <input type="text" id="pw" name="password" class="adv_search_txtbox" placeholder="Day" pattern="[1-31]" style="width: 25%;">
-                </span>
+                <input type="text" id="pw" name="dob_y" class="adv_search_txtbox" placeholder="Year" pattern="[1|2]{1}[0-9]{1}[0-9]{1}[0-9]{1}" style="width: 25%;">
+                <input type="text" id="pw" name="dob_m" class="adv_search_txtbox" placeholder="Month" pattern="[1-12]+" style="width: 25%;">
+                <input type="text" id="pw" name="dob_d" class="adv_search_txtbox" placeholder="Day" pattern="[1-31]+" style="width: 25%;">
                 <div class="adv_search_subtitle">Gender</div>
+                <span>
                 <input type="radio" name="gender" value="male" checked> Male
                 <input type="radio" name="gender" value="female"> Female
-                <input type="radio" name="gender" value="any"> Ignore Gender
-                <span>
+                <input type="radio" name="gender" value="any" checked> Ignore Gender
+                </span>
 
             </div>
 
             <div class="dbox" style="background-color: #fff; overflow:hidden; ">
                 <div style="width: 50%;float: left;">
                     <div class="adv_search_subtitle">Hometown</div>
-                    <select class="adv_search_dropdown">
-                        <option value="Unset" selected>Unset</option>
-                        <option value="Jaffna">Jaffna</option>
-                        <option value="Kilinochchi">Kilinochchi</option>
-                        <option value="Mannar">Mannar</option>
-                        <option value="Mullaitivu">Mullaitivu</option>
-                        <option value="Vavuniya">Vavuniya</option>
-                        <option value="Puttalam">Puttalam</option>
-                        <option value="Kurunegala">Kurunegala</option>
-                        <option value="Gampaha">Gampaha</option>
-                        <option value="Colombo">Colombo</option>
-                        <option value="Kalutara">Kalutara</option>
-                        <option value="Anuradhapura">Anuradhapura</option>
-                        <option value="Polonnaruwa">Polonnaruwa</option>
-                        <option value="Matale">Matale</option>
-                        <option value="Kandy">Kandy</option>
-                        <option value="Nuwara Eliya">Nuwara Eliya</option>
-                        <option value="Kegalle">Kegalle</option>
-                        <option value="Ratnapura">Ratnapura</option>
-                        <option value="Trincomalee">Trincomalee</option>
-                        <option value="Batticaloa">Batticaloa</option>
-                        <option value="Ampara">Ampara</option>
-                        <option value="Badulla">Badulla</option>
-                        <option value="Monaragala">Monaragala</option>
-                        <option value="Hambantota">Hambantota</option>
-                        <option value="Matara">Matara</option>
-                        <option value="Galle">Galle</option>
+                    <select name="hometown" class="adv_search_dropdown">
+                        <option value="-1" selected>Unset</option>
+                        <?php
+                        $query32 = "SELECT * FROM district ORDER BY name asc;";
+                        $result = mysqli_query($conn,$query32);
+                        if (mysqli_num_rows($result)){
+                            while ($row_qt =  mysqli_fetch_assoc($result)){
+                                echo "<option value='".$row_qt['name']."'>".$row_qt['name']."</option>";
+                            }
+                        }
+                        ?>
                     </select>
                 </div>
                 <div style="width: 50%;float: left">
                     <div class="adv_search_subtitle">Current City</div>
-                    <select class="adv_search_dropdown">
-                        <option value="Unset" selected>Unset</option>
-                        <option value="Jaffna">Jaffna</option>
-                        <option value="Kilinochchi">Kilinochchi</option>
-                        <option value="Mannar">Mannar</option>
-                        <option value="Mullaitivu">Mullaitivu</option>
-                        <option value="Vavuniya">Vavuniya</option>
-                        <option value="Puttalam">Puttalam</option>
-                        <option value="Kurunegala">Kurunegala</option>
-                        <option value="Gampaha">Gampaha</option>
-                        <option value="Colombo">Colombo</option>
-                        <option value="Kalutara">Kalutara</option>
-                        <option value="Anuradhapura">Anuradhapura</option>
-                        <option value="Polonnaruwa">Polonnaruwa</option>
-                        <option value="Matale">Matale</option>
-                        <option value="Kandy">Kandy</option>
-                        <option value="Nuwara Eliya">Nuwara Eliya</option>
-                        <option value="Kegalle">Kegalle</option>
-                        <option value="Ratnapura">Ratnapura</option>
-                        <option value="Trincomalee">Trincomalee</option>
-                        <option value="Batticaloa">Batticaloa</option>
-                        <option value="Ampara">Ampara</option>
-                        <option value="Badulla">Badulla</option>
-                        <option value="Monaragala">Monaragala</option>
-                        <option value="Hambantota">Hambantota</option>
-                        <option value="Matara">Matara</option>
-                        <option value="Galle">Galle</option>
+                    <select name="currentcity" class="adv_search_dropdown">
+                        <option value="-1" selected>Unset</option>
+                        <?php
+                        $query32 = "SELECT * FROM district ORDER BY name asc;";
+                        $result = mysqli_query($conn,$query32);
+                        if (mysqli_num_rows($result)){
+                            while ($row_qt =  mysqli_fetch_assoc($result)){
+                                echo "<option value='".$row_qt['name']."'>".$row_qt['name']."</option>";
+                            }
+                        }
+                        ?>
                     </select>
                 </div>
 
@@ -139,7 +110,7 @@
                 <input type="button" onclick="insertSkill();" class="add_new_item_btn" value="Add Skill">
                 <div style="clear: both;"></div>
             </div>
-            <input type="submit" class="adv_srch_float_btn" value="Advanced Search`">
+            <input type="submit" class="adv_srch_float_btn" value="Advanced Search">
             <div style="clear: both;"></div>
         </div>
 
