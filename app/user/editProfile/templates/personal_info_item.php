6652f226a25abac6613cd424292f2958928c7fc8
Emalsha <remalsha@gmail.com>
2017-01-14 23:56:17 +0530

    older profile edit

---
+++ app/user/editProfile/templates/personal_info_item.php
@@ -0,0 +1,11 @@
+<div class="contact_info_item edit_profile_contactinfo_item">
+	<div class="edit_profile_contactinfo_item_field">School :</div>
+	<div class="edit_profile_contactinfo_item_remove" onclick='this.parentElement.outerHTML="";'></div>
+	<select class="edit_profile_privacy_drop_box">
+		<option value="volvo">Everyone</option>
+		<option value="saab">Students</option>
+		<option value="mercedes">Instructors</option>
+		<option value="audi">Lecturers</option>
+	</select>
+	<div class="edit_profile_contactinfo_item_value">St. Aloysius' College, Galle</div>
+</div>
---
+++ app/user/editProfile/templates/socities.php
@@ -0,0 +1,21 @@
+<div class="dbox profile_section_socities">
+	<div class="dboxheader dbox_head_profile_socities">
+		<div class="dboxtitle botmarg">
+			Roles in Clubs and Socities
+		</div>
+	</div>
+	<?php include("socity_item.php") ?>
+	<?php include("socity_item.php") ?>
+	<?php include("socity_item.php") ?>
+	<?php include("socity_item.php") ?>
+	<?php include("socity_item.php") ?>
+	<?php include("socity_item.php") ?>
+	<?php include("socity_item.php") ?>
+	<?php include("socity_item.php") ?>
+	<?php include("socity_item.php") ?>
+	<?php include("socity_item.php") ?>
+	<?php include("socity_item.php") ?>
+	<?php include("socity_item.php") ?>
+	<?php include("socity_item.php") ?>
+	<?php include("socity_item.php") ?>
+</div>
